577f0036a759efcdb4902add0016a9b49e8265b8
Muhammad Huzaifa <mhuzaifa91@gmail.com>
2025-04-29 08:57:25 +0500

    Add URL routing for securechatapp and enhance CustomUser model with additional fields and manager methods

--- Backend/cnproject/securechatapp/models.py
+++ Backend/cnproject/securechatapp/models.py
@@ -1,6 +1,28 @@
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager, Group, Permission
 from django.db import models
+from django.contrib.auth.models import BaseUserManager
+from django.utils import timezone
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, username, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, username, password, **extra_fields)
+    
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     id = models.AutoField(primary_key=True)
     email = models.EmailField(unique=True)
@@ -8,6 +30,12 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     fullname = models.CharField(max_length=300, unique=False, default='')
     is_online = models.BooleanField(default=False)
     last_seen = models.DateTimeField(null=True, blank=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    
+    date_joined = models.DateTimeField(default=timezone.now)
 
     groups = models.ManyToManyField(
         Group,
@@ -22,6 +50,10 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
+
+    objects = CustomUserManager()    
+    def __str__(self):
+        return self.email
 
 class ChatRoom(models.Model):
     id = models.AutoField(primary_key=True)
