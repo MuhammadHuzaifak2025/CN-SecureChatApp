0280fcf88f8aa9d7fbbc09017eef861550908724
Muhammad Huzaifa <mhuzaifa91@gmail.com>
2025-05-04 01:55:22 +0500

    Add sender username to chat message handling for better context

--- Backend/cnproject/securechatapp/consumer.py
+++ Backend/cnproject/securechatapp/consumer.py
@@ -262,7 +262,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
                     else:
                         message['content'] = EncryptionManager.decrypt_message(message, recipents_private_key)
                 message['timestamp'] = message['timestamp'].strftime("%Y-%m-%d %H:%M:%S")
-                message['sender'] = CustomUser.objects.get(id=message['sender']).username
+                if(message['sender'] == current_user_id):
+                    message['sender'] = self.scope['user'].username
+                else:
+                    message['sender'] = self.scope['url_route']['kwargs']['chatwithusername']
                 message_list.append(message)
             except Exception as e:
                 print(f"Decryption error for message {message['id']}: {e}")
